3db7e24fd1c41b5186a52816b8b760e83bee8402
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-04-16 04:45:59 +0300

    example change css attributes

---
+++ 36/main.js
@@ -0,0 +1,10 @@
+const title = document.getElementById('page-title');
+title.setAttribute("style", "position: relative;");
+
+title.style.color = "red";
+
+title.style.top = "70px";
+
+title.style.left = "70px";
+
+title.style.backgroundColor = "coral";
