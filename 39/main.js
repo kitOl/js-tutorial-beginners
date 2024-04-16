39471aa6e70a31d5c78996758146073ad8825b92
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-04-16 16:26:16 +0300

    example js events

---
+++ 39/main.js
@@ -0,0 +1,9 @@
+const title = document.getElementById("page-title");
+
+title.onclick = function () {
+  console.log("click");
+};
+
+title.onmouseover = function () {
+  console.log("hovered");
+};
