691094fb6cb6b32a45ef2aa1b8718b48fe53e6b4
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-04-16 18:01:13 +0300

    example onclick event code

---
+++ 40/test.js
@@ -0,0 +1,12 @@
+const content = document.getElementById("content");
+const button = document.getElementById("show-more");
+
+button.onclick = function () {
+  if (content.classList == "open") {
+    content.className = "";
+    button.innerHTML = "Show more";
+  } else {
+    content.className = "open";
+    button.innerHTML = "Show less";
+  }
+};
